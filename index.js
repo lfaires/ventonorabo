e4c2f84b45c546a018313b77879ff7b7f4daf273
lfaires-revo <laires@revoenergia.com.br>
2024-01-31 18:13:54 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"Parabéns, você ganhou US$ 100,00!!!! Faça resgate do valor no dia 12/06/2024 às 12PM"
+
+function showMessage() {
+    const button = document.getElementById('btn')
+    const message = document.getElementById('message')
+    button.style = "display: none"
+    message.style = "display: block"
+}
